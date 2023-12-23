e0a5cf1fb7b8dfda94f6fe97951279ae4a7ef0d6
Tim-Siu <e1088140@u.nus.edu>
2023-12-23 11:23:56 +0800

    Fix bugs

--- datasets/penn_fudan.py
+++ datasets/penn_fudan.py
@@ -10,40 +10,43 @@ class PennFudanDataset(torch.utils.data.Dataset):
     def __init__(self, root, transforms):
         self.root = root
         self.transforms = transforms
+        # load all image files, sorting them to
+        # ensure that they are aligned
         self.imgs = list(sorted(os.listdir(os.path.join(root, "PNGImages"))))
         self.masks = list(sorted(os.listdir(os.path.join(root, "PedMasks"))))
 
     def __getitem__(self, idx):
+        # load images and masks
         img_path = os.path.join(self.root, "PNGImages", self.imgs[idx])
         mask_path = os.path.join(self.root, "PedMasks", self.masks[idx])
         img = read_image(img_path)
         mask = read_image(mask_path)
-        # torch.unique can be used to get unique values in a tensor
+        # instances are encoded as different colors
         obj_ids = torch.unique(mask)
-        # torch.unique will sort the values and as a convention the first value is the background
-        # remove the background
+        # first id is the background, so remove it
         obj_ids = obj_ids[1:]
         num_objs = len(obj_ids)
-        
-        # split the color-encoded mask into a set of binary masks
-        masks = (mask == obj_ids[:, None, None]).to(dtype=mask.dtype)
+
+        # split the color-encoded mask into a set
+        # of binary masks
+        masks = (mask == obj_ids[:, None, None]).to(dtype=torch.uint8)
 
         # get bounding box coordinates for each mask
         boxes = masks_to_boxes(masks)
 
+        # there is only one class
         labels = torch.ones((num_objs,), dtype=torch.int64)
 
         image_id = idx
-        # calculate the area of the bounding boxes
         area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
-        # suppose all instances do not overlap or they are not crowd
+        # suppose all instances are not crowd
         iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
 
+        # Wrap sample and targets into torchvision tv_tensors:
         img = tv_tensors.Image(img)
 
         target = {}
-        target["boxes"] = tv_tensors.BoundingBoxes(boxes, format="XYXY",
-                                                   canvas_size=img.size)
+        target["boxes"] = tv_tensors.BoundingBoxes(boxes, format="XYXY", canvas_size=F.get_size(img))
         target["masks"] = tv_tensors.Mask(masks)
         target["labels"] = labels
         target["image_id"] = image_id
@@ -54,7 +57,6 @@ class PennFudanDataset(torch.utils.data.Dataset):
             img, target = self.transforms(img, target)
 
         return img, target
-    
+
     def __len__(self):
         return len(self.imgs)
-    
