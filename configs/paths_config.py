b884042c5acb01873f91817ae290d44671735574
Tim-Siu <e1088140@u.nus.edu>
2023-12-23 11:37:58 +0800

    Correct output path

--- configs/paths_config.py
+++ configs/paths_config.py
@@ -1,2 +1,7 @@
+import os
+
 data_root = '/temp/data/PennFudanPed'
-save_path = '~/VisionFinetuning/results'
+save_path = os.path.expanduser('~/VisionFinetuning/results')
+
+if not os.path.exists(save_path):
+    os.makedirs(save_path, exist_ok=True)
